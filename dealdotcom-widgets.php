ecfa6e4124d8387e33de3dda64731e4245566aff
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-01-23 11:37:35 +0000

    bump versions
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/dealdotcom-widgets/trunk@635 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- dealdotcom-widgets.php
+++ dealdotcom-widgets.php
@@ -3,7 +3,7 @@
 Plugin Name: Dealdotcom Widgets
 Plugin URI: http://www.semiologic.com/software/marketing/dealdotcom/
 Description: Widgets to display <a href="http://go.semiologic.com/dealdotcom">dealdotcom</a>'s deal of the day.
-Version: 1.1.3 alpha
+Version: 1.1.3 RC
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 */
