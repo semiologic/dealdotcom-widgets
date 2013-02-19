be59e4825d69bca2afbe878d43035a99c295f62b
Mike Koepke <mike.koepke@gmail.com>
2013-02-19 13:44:34 -0600

    This plugin is no longer being maintained

--- dealdotcom-widgets.php
+++ dealdotcom-widgets.php
@@ -1,284 +1,31 @@
 <?php
 /*
-Plugin Name: Dealdotcom Widgets
-Plugin URI: http://www.semiologic.com/software/dealdotcom/
-Description: Widgets that let you display <a href="http://go.semiologic.com/dealdotcom">Dealdotcom</a>'s deal of the day.
-Version: 2.0
-Author: Denis de Bernardy
-Author URI: http://www.getsemiologic.com
-Text Domain: deadotcom-widgets
-Domain Path: /lang
+Version: 1.0.1
 */
+// obsolete file
 
-/*
-Terms of use
-------------
+$active_plugins = get_option('active_plugins');
 
-This software is copyright Mesoconcepts (http://www.mesoconcepts.com), and is distributed under the terms of the GPL license, v.2.
+if ( !is_array($active_plugins) )
+{
+	$active_plugins = array();
+}
 
-http://www.opensource.org/licenses/gpl-2.0.php
-**/
+foreach ( (array) $active_plugins as $key => $plugin )
+{
+	if ( $plugin == 'dealdotcom-widgets.php' )
+	{
+		unset($active_plugins[$key]);
+		break;
+	}
+}
 
+if ( !in_array('dealdotcom-widgets/dealdotcom-widgets.php', $active_plugins) )
+{
+	$active_plugins[] = 'dealdotcom-widgets/dealdotcom-widgets.php';
+}
 
-load_plugin_textdomain('dealdotcom-widgets', false, dirname(plugin_basename(__FILE__)) . '/lang');
+sort($active_plugins);
 
-
-/**
- * dealdotcom
- *
- * @package DealDotCom Widgets
- **/
-
-class dealdotcom extends WP_Widget {
-	/**
-	 * init()
-	 *
-	 * @return void
-	 **/
-
-	function init() {
-		if ( get_option('widget_dealdotcom') === false ) {
-			foreach ( array(
-				'dealdotcom_widgets' => 'upgrade',
-				'dealdotcom' => 'upgrade_1_0',
-				) as $ops => $method ) {
-				if ( get_option($ops) !== false ) {
-					$this->alt_option_name = $ops;
-					add_filter('option_' . $ops, array(get_class($this), $method));
-					break;
-				}
-			}
-		}
-	} # init()
-	
-	
-	/**
-	 * widgets_init()
-	 *
-	 * @return void
-	 **/
-
-	function widgets_init() {
-		register_widget('dealdotcom');
-	} # widgets_init()
-	
-	
-	/**
-	 * dealdotcom()
-	 *
-	 * @return void
-	 **/
-
-	function dealdotcom() {
-		$widget_ops = array(
-			'classname' => 'dealdotcom',
-			'description' => __('Dealdotcom\'s deal of the day', 'dealdotcom-widgets'),
-			);
-		$control_ops = array(
-			'width' => 330,
-			);
-		
-		$this->init();
-		$this->WP_Widget('dealdotcom', __('Dealdotcom Widget', 'dealdotcom-widgets'), $widget_ops, $control_ops);
-	} # dealdotcom()
-	
-	
-	/**
-	 * widget()
-	 *
-	 * @param array $args
-	 * @param array $instance
-	 * @return void
-	 **/
-
-	function widget($args, $instance) {
-		if ( is_admin() )
-			return;
-		
-		extract($args, EXTR_SKIP);
-		$instance = wp_parse_args($instance, dealdotcom::defaults());
-		extract($instance, EXTR_SKIP);
-		
-		if ( !$aff_id ) {
-			echo $before_widget
-				. '<div style="border: solid 2px firebrick; padding: 5px; background-color: AntiqueWhite; color: firebrick; font-weight: bold;">'
-				. sprintf(__('Your <a href="%s">dealdotcom</a> affiliate ID is not configured.', 'dealdotcom-widgets'), esc_url('http://go.semiologic.com/dealdotcom'))
-				. '</div>'
-				. $after_widget;
-			return;
-		}
-		
-		$deal = get_transient('dealdotcom_deal');
-		
-		if ( !$deal )
-			$deal = dealdotcom::update_deal();
-		
-		if ( !$deal )
-			return;
-		
-		if ( is_ssl() )
-			$deal['image'] = str_replace('http://', 'https://', $deal['image']);
-		
-		
-		$plugin_path = plugin_dir_url(__FILE__);
-		
-		echo $before_widget
-			. '<div style="width: 148px; margin: 0px auto; border: solid 2px orange; text-align: center;">'
-			. '<a href="' . esc_url('http://dealdotcom.com/invite/' . intval($aff_id)) . '"'
-				. ' title="' . esc_attr($deal['name'] . ' @ $' . $deal['price']) . '"'
-				. ' rel="nofollow"'
-				. '>'
-			. '<img src="' . esc_url($plugin_path . 'dealdotcom-top.gif') . '" alt="" />'
-			. '<br />'
-			. '<img src="' . esc_url($deal['image']) . '" border="0"'
-				. ' alt="' . esc_attr($deal['name'] . ' @ $' . $deal['price']) . '"'
-				. ' style="width: 148px; margin: 0px auto;"'
-				. '/>'
-			. '<br />'
-			. '<img src="' . esc_url($plugin_path . 'dealdotcom-bottom.gif') . '" alt="" />'
-			. '</a>'
-			. '</div>'
-			. $after_widget;
-	} # widget()
-	
-	
-	/**
-	 * update_deal()
-	 *
-	 * @return void
-	 **/
-
-	function update_deal() {
-		$url = 'http://www.dealdotcom.com/wp';
-		
-		$deal = wp_remote_fopen($url);
-		
-		if ( $deal ) {
-			list($name, $price, $image) = split("<br>", $deal);
-			$name = trim($name);
-			$price = trim($price);
-			$image = trim($image);
-			$deal = compact('name', 'price', 'image');
-		}
-
-		set_transient('dealdotcom_deal', $deal);
-		
-		if ( !wp_next_scheduled('dealdotcom_update') ) {
-			wp_schedule_event(time(), 'hourly', 'dealdotcom_update');
-			
-			# clear corrup cron job
-			wp_clear_scheduled_hook('dealdotcom');
-		}
-
-		return $deal;
-	} # update_deal()
-	
-	
-	/**
-	 * update()
-	 *
-	 * @param array $new_instance
-	 * @param array $old_instance
-	 * @return array $instance
-	 **/
-
-	function update($new_instance, $old_instance) {
-		$instance['aff_id'] = strip_tags($new_instance['aff_id']);
-		
-		return $instance;
-	} # update()
-	
-	
-	/**
-	 * form()
-	 *
-	 * @param array $instance
-	 * @return void
-	 **/
-
-	function form($instance) {
-		$instance = wp_parse_args($instance, dealdotcom::defaults());
-		extract($instance, EXTR_SKIP);
-		
-		echo '<p>'
-			. sprintf(__('Your <a href="%s">DealDotCom</a> Affiliate ID:', 'dealdotcom-widgets'), esc_url('http://go.semiologic.com/dealdotcom'))
-				. '<br />'
-			. '<label>'
-			. 'http://www.dealdotcom.com/invite/'
-			. '<input class="widefat" style="width: 50px;"'
-			. ' name="' . $this->get_field_name('aff_id') . '"'
-			. ' type="text" value="' . esc_attr($aff_id) . '"'
-			. ' />'
-			. '</label>'
-			. '</p>' . "\n";
-	} # form()
-	
-	
-	/**
-	 * defaults()
-	 *
-	 * @return void
-	 **/
-
-	function defaults() {
-		return array(
-			'aff_id' => '',
-			);
-	} # defaults()
-	
-	
-	/**
-	 * upgrade()
-	 *
-	 * @param array $ops
-	 * @return array $ops
-	 **/
-
-	function upgrade($ops) {
-		$widget_contexts = class_exists('widget_contexts')
-			? get_option('widget_contexts')
-			: false;
-		
-		foreach ( $ops as $k => $o ) {
-			$ops[$k] = array(
-				'aff_id' => $o['aff_id']
-				);
-			if ( isset($widget_contexts['dealdotcom-' . $k]) ) {
-				$ops[$k]['widget_contexts'] = $widget_contexts['dealdotcom-' . $k];
-			}
-		}
-		
-		return $ops;
-	} # upgrade()
-	
-	
-	/**
-	 * upgrade_1_0()
-	 *
-	 * @param array $ops
-	 * @return array $ops
-	 **/
-
-	function upgrade_1_0($ops) {
-		$widget_contexts = class_exists('widget_contexts')
-			? get_option('widget_contexts')
-			: false;
-		
-		if ( !empty($ops['aff_id']) ) {
-			$ops = array(
-				'aff_id' => $ops['aff_id']
-				);
-			if ( isset($widget_contexts['dealdotcom-2']) ) {
-				$ops['widget_contexts'] = $widget_contexts['dealdotcom-2'];
-			}
-		} else {
-			$ops = array();
-		}
-		
-		return $ops;
-	} # upgrade_1_0()
-} # dealdotcom
-
-add_action('widgets_init', array('dealdotcom', 'widgets_init'));
-add_action('dealdotcom_update', array('dealdotcom', 'update_deal'));
+update_option('active_plugins', $active_plugins);
 ?>
--- readme.txt
+++ readme.txt
@@ -1,21 +1,15 @@
 === DealDotCom Widgets ===
 Contributors: Denis-de-Bernardy
-Donate link: http://www.semiologic.com/partners/
-Tags: dealdotcom, marketing, internet-marketing, monetize, money, adsense, widget, semiologic
+Tags:
 Requires at least: 2.8
-Tested up to: 3.2.1
+Tested up to: 3.5.1
 Stable tag: trunk
 
-Dealdotcom is a great place to look for deals on internet marketing related materials.
 
 
 == Description ==
 
-The Dealdotcom plugin for WordPress introduces widgets that let you insert [Dealdotcom](http://go.semiologic.com/dealdotcom)'s deal of the day on your site.
-
-In the event you're unfamiliar with it, Dealdotcom is a great place to look for deals on internet marketing related materials, and this widget is a great way to collect commissions if you run a site related to the topic.
-
-After activating the plugin, browse Appearance / Widgets. Insert one or more Dealdotcom widgets, configure your affiliate ID in each, and you're done.
+No longer supported
 
 = Help Me! =
 
@@ -27,14 +21,9 @@ If you require more dedicated assistance, consider using [Semiologic Pro](http:/
 == Installation ==
 
 1. Upload the plugin folder to the `/wp-content/plugins/` directory
-1. Activate the plugin through the 'Plugins' menu in WordPress
+2. Activate the plugin through the 'Plugins' menu in WordPress
 
 
 == Change Log ==
 
-= 2.0 =
-
-- Localization
-- SSL support
-- WP_Widget class
-- Code enhancements and optimizations
+No longer supported
