a47a6e23e95e8a20bf5f14a02bbd2dd029d47441
Mike Koepke <mike.koepke@gmail.com>
2013-03-19 15:54:48 -0500

    Remove this widget

--- dealdotcom-widgets-info.php
+++
@@ -1,4 +0,0 @@
-<?php
-__('Dealdotcom Widgets', 'dealdotcom-widgets');
-__('Widgets that let you display <a href="http://go.semiologic.com/dealdotcom">Dealdotcom</a>\'s deal of the day.', 'dealdotcom-widgets')
-?>
